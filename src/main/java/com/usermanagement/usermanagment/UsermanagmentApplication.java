6ddc30b2decabeb55f379d3ca1180ed505decf5e
itsdevsyed <shoaib.syed4@outlook.com>
2025-05-26 21:33:51 +0530

    added something which has no mmmeaning

--- src/main/java/com/usermanagement/usermanagment/UsermanagmentApplication.java
+++ src/main/java/com/usermanagement/usermanagment/UsermanagmentApplication.java
@@ -8,7 +8,7 @@ public class UsermanagmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsermanagmentApplication.class, args);
-		System.out.println(" databased is working for now ");
+		System.out.println(" databased is Connected to the and work perfectly fine ");
 	}
 
 }
